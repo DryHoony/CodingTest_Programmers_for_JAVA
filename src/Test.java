c3372f5d4c075c7aa4ce7c97ba7fdf9c2a8952f4
Hoony <xognsdl710@naver.com>
2023-02-17 22:54:47 +0900

    2023.02.17 코테연습

--- src/Test.java
+++ src/Test.java
@@ -3,58 +3,32 @@ import programmers.level0.ReverseArray;
 
 public class Test {
 
-    public int gcd(int a, int b){
-        if(a%b==0) return b;
-        return gcd(b,a%b);
-    }
-
     public static void main(String[] args) {
-        Test t = new Test();
 
-        int n=30;
-        int r=15;
+//        int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
+//        int n = 2;
+        int[] num_list = {100, 95, 2, 4, 5, 6, 18, 33, 948};
+        int n = 3;
 
-        // 분수(fraction)
-        int[] numArray = new int[r]; // 분자(numerator),
-        int[] denArray = new int[r]; // 분모(denominator)
+        int l = num_list.length/n;
+        int[][] answer = new int[l][n];
 
-        for (int i = 0; i < r; i++) {
-            numArray[i] = n-i;
-            denArray[i] = r-i;
-        }
-        // 생성완료
-
-        int j;
-        int g;
-        for (int i = 0; i < r; i++) { //분모(den)의 index
-
-            // 분모(den)의 인자에 대해서 분자(num)에서 약분을 시행한다.
-            j=0; // 초기화
-            while(denArray[i]!=1){
-                g = t.gcd(numArray[j],denArray[i]);
-                if(g>=2){ // 약분이 된다면 - 나누어 떨어지는 것이 아닌 gcd가 2이상이면
-
-                    numArray[j] /= g;
-                    denArray[i] /= g;
-                }
-
-                j++;
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = num_list[n*i+j];
             }
+
         }
 
-        // 분자array의 인자를 모두 곱한 값 return
-        int answer = 1;
-        for(int na:numArray){
-            answer *= na;
-        }
-        System.out.println(answer);
-
+        System.out.println();
 
 
 
 
 
     }
+
+    
 
 
 }
