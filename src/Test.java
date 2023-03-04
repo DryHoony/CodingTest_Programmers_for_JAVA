09311557efd330204648ef5704c7bcc9d80de208
Hoony <xognsdl710@naver.com>
2023-03-05 01:10:04 +0900

    2023.03.05 코테연습 및 파일정리

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Test {
 
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
 
 
---
+++ src/programmers/level1/LeastSquare.java
@@ -0,0 +1,30 @@
+package programmers.level1;
+
+public class LeastSquare { // 최소직사각형
+
+    public static int[] maxfirst(int[] list){
+        if (list[0]>=list[1]) return list;
+        else{
+            int[] answer = {list[1],list[0]};
+            return answer;
+        }
+    }
+
+    public static int[] whUpdate(int[] a, int[] b){
+        return new int[] {Math.max(a[0],b[0]),Math.max(a[1],b[1])};
+    }
+
+    public static void main(String[] args) {
+        int[][] sizes = {{60,50},{30,70},{60,30},{80,40}};
+        int[] answer = maxfirst(sizes[0]);
+
+        for (int i = 1; i < sizes.length; i++) {
+            answer = whUpdate(answer, maxfirst(sizes[i]));
+        }
+
+        System.out.println(answer[0]*answer[1]);
+
+
+
+    }
+}
