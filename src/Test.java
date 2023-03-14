4ef1690449b2643f7ebb41f9439ce3ffc4ef3f04
Hoony <xognsdl710@naver.com>
2023-03-14 15:35:20 +0900

    2023.03.14 코테연습 및 파일정리

--- src/Test.java
+++ src/Test.java
@@ -3,95 +3,32 @@ import java.util.*;
 public class Test {
 
 
-    public static void main(String[] args) { // ver4 - ver3의 test 2,15실패 개선
-//        장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다. >> 가 원인인 듯
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
-//        String[] genres = {"classic", "pop", "classic", "classic"};
-//        int[] plays = {500, 600, 150, 800};
-        // 장르내에 노래가 하나만 있을경우를 고려!!
+    public static void main(String[] args) {
 
-        int l = genres.length;
+        Stack<String> stack = new Stack<>();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.push("d");
+        System.out.println(stack);
 
-        // 장르별 데이터 구분
-        Map<String,Map<Integer,Integer>> genreDetail = new HashMap<>(); // 해당장르의 Map<plays, Index>를 가진다.
-        // !!!오류!!! 특정장르의 plays 에 중복이 존재하면? >> key 값으로 사용할 수 없다! 본질적인 오류!
-        Map<String,Integer> genrePlayTime = new HashMap<>(); // 해당장르의 총 playTime
-        for (int i = 0; i < l; i++) {
+        System.out.println("pop연산 " + stack.pop() + stack);;
+        System.out.println("peek연산 " + stack.peek() + stack);;
+        System.out.println();
 
-            if (genreDetail.containsKey(genres[i])){ // 해당장르 있으면 추가
-                genreDetail.get(genres[i]).put(plays[i],i);
-            }else{ // 해당장르 없으면 새롭게 할당
-                Map<Integer,Integer> newMap = new HashMap<>(); // 장르마다 새로 생성하면 새로운 객체?(다른 참조값?)
-                newMap.put(plays[i],i);
-                genreDetail.put(genres[i], newMap); // 같은값을 참조하여, 중복되기 싶다!! 추후 자세히 검토 필요!
-//                newMap.clear();
-            }
+        System.out.println(stack + "에서 없는값 검색 결과 "+ stack.search("k"));
+        System.out.println("search(a) 결과 " + stack.search("a"));
+        System.out.println("search(b) 결과 " + stack.search("b"));
+        System.out.println("search(c) 결과 " + stack.search("c"));
 
-            if (genrePlayTime.containsKey(genres[i])){  // 해당장르 있으면 시간 추가
-                genrePlayTime.put(genres[i], plays[i] + genrePlayTime.get(genres[i]));
-            }else{ // 해당장르 없으면 시간 새롭게 할당
-                genrePlayTime.put(genres[i],plays[i]);
-            }
-        }
-        System.out.println("genreDetail = " + genreDetail); // ok
-        System.out.println("genrePlayTime = " + genrePlayTime);
+        System.out.println("get(0) 결과 " + stack.get(0));
+        System.out.println("get(2) 결과 " + stack.get(2));
+        System.out.println("get(3) 결과는 ArrayIndexOutOfBoundsException ");
 
-        // 1. 속한노래가 많이 재생된 장르순서 뽑기(중복X)
-        List<String> genreRank = new ArrayList<>(); // 장르 순위 판별
-        int i;
-        for(String genre:genrePlayTime.keySet()){
-            i=0;
-            while (i<genreRank.size()){
-                if(genrePlayTime.get(genreRank.get(i)) > genrePlayTime.get(genre))
-                    i++;
-                else break;
-            }
-            genreRank.add(i,genre); // 로직 검증필요!
-        }
-
-        // 장르순위 확인 test
-        System.out.println("genreRank = " + genreRank);
-
-        // 2. 1.에서 뽑힌 장르순서로, 장르 내 많이 재생된 노래를 두 개 수록
-
-        List<Integer> answer = new ArrayList<>();
-        int a,b;
-
-        for(String genre:genreRank){
-
-            Integer[] top12 = genreDetail.get(genre).keySet().toArray(new Integer[0]);
-            if(top12.length==1){ // 장르에 곡이 하나라면 하나만 수록(예외처리) >> ok
-                answer.add(genreDetail.get(genre).get(top12[0]));
-            }
-            else{
-                a=0;
-                b=0;
-                for (int n:top12){
-                    if(n>a){
-                        b=a;
-                        a=n;
-                    } else if (n>b) {
-                        b=n;
-                    }
-                }
-                // 3. 2.에서 수록할 때, 장르 내 재생 횟수가 같으면(중복이면) 고유 번호가 낮은 노래 먼저 >> !!Working on It
-                if(a==b){
-                    if(genreDetail.get(genre).get(a) < genreDetail.get(genre).get(b)){
-                        answer.add(genreDetail.get(genre).get(a));
-                        answer.add(genreDetail.get(genre).get(b));
-                    }else{
-                        answer.add(genreDetail.get(genre).get(b));
-                        answer.add(genreDetail.get(genre).get(a));
-                    }
-                }else{
-                    answer.add(genreDetail.get(genre).get(a));
-                    answer.add(genreDetail.get(genre).get(b));
-                }
-            }
-        }
-
-        System.out.println(answer);
+        System.out.println();
+        System.out.println(stack + "비어있나? " + stack.empty());
+        stack.clear();
+        System.out.println("clear 후에는 비어있나? " + stack.empty());
 
 
     }
--- src/programmers/highScoreKit/StackQueue/AboutStackAndQueue.java
+++ src/programmers/highScoreKit/StackQueue/AboutStackAndQueue.java
@@ -1,9 +1,128 @@
 package programmers.highScoreKit.StackQueue;
 
+import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TransferQueue;
+
 public class AboutStackAndQueue {
 //    Stack, Queue, LIFO, FIFO, push & pop
 
+    public static void main1(String[] args) {
+        System.out.println("Stack");
+        // 스택 정의
+//        - 제한적으로 접근할 수 있는 나열 구조, 접근은 언제나 목록의 끝에서만 일어난다, 끝먼저내기 목록(pushdouwn list)
+//        - LIFO : Last In First Out , 후입선출,
 
+        // 스택 특징
+//        - '정적 구현'은 1차원 배열을 사용하며 크기가 제한되어 있다.(isfull 함수 존재)
+//          배열과 달리 상수 시간에 i번째 요소에 접근 할 수 없다. But, 데이터 추가나 삭제는 상수 시간에 가능하다
+//          한 프로그램이 사용하는 스택영역은 기본적으로 크기가 고정되어 있다. >> Stack Buffer OverFlow 주의!
+//        - '동적 구현'은 연결리스트를 사용한다. 크기 제한이 없지만 구현이 복잡하며, 삽입/ 삭제 시간이 오래 걸린다.
+
+        // 사용예시
+//        - 재귀 알고리즘에서 함수를 호출하는 경우 임시 데이터를 스택에 넣어준다. 퇴각 검색(backTrack)시 임시 데이터를 빼준다.
+//        - 웹 브라우저 방문기록(뒤로가기), 실행 취소(undo), 역순, 후위 표기법
+//        - 시스템 스택(system stack) : 운영체제가 사용, 함수 호출 시 활성 레코드(activation record)가 만들어지며, 복귀 주소가 저장됨
+
+
+        // 스택 기능
+//        - Push : 가장 윗 데이터로 입력
+//        - Pop : 가장 윗 데이터로 반환(반환 후 해당 데이터 삭제)
+//        - Top, Peek : 가장 윗 데이터 반환, 탑 포인터가 가리키는 데이터를 조회
+//        - Empty, isEmpty : 스택 비었다면 1반환(Java 에서는 true 반환) , else 0반환
+
+        Stack<String> stack = new Stack<>();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.push("d");
+        System.out.println(stack);
+
+        System.out.println("pop연산 " + stack.pop() + stack);;
+        System.out.println("peek연산 " + stack.peek() + stack);;
+        System.out.println();
+
+        System.out.println(stack + "에서 없는값 검색 결과 "+ stack.search("k"));
+        System.out.println("search(a) 결과 " + stack.search("a"));
+        System.out.println("search(b) 결과 " + stack.search("b"));
+        System.out.println("search(c) 결과 " + stack.search("c"));
+
+        System.out.println("get(0) 결과 " + stack.get(0));
+        System.out.println("get(2) 결과 " + stack.get(2));
+        System.out.println("get(3) 결과는 ArrayIndexOutOfBoundsException ");
+
+        System.out.println();
+        System.out.println(stack + "비어있나? " + stack.empty());
+        stack.clear();
+        System.out.println("clear 후에는 비어있나? " + stack.empty());
+
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Queue");
+
+//        큐 정의
+//        - 먼저 들어온 것이 먼저 나간다. 입력순서대로 출력
+//        - 한쪽 끝에서만 삽입만, 다른 한쪽끝에서는 삭제 연산만 이루어지는 유한 순서 리스트
+//        - FIFO : First In First Out
+
+//        큐 특징
+//        - 선형 큐 : 배열을 사용해서 구현, 크기가 제한되 있고, 빈 공간을 사용하려면 모든 자료를 꺼내거나 빈 공간을 한 칸씩 옮겨야 하는 단점이 있다.
+//          Enqueue, Dequeue 할 때마다 데이터가 앞으로 밀려나는 문제 발생
+//        - 환형 큐(원형 큐) : front가 큐의 끝에 닿으면 큐의 맨 앞으로 자료를 보내어 원형으로 연결 하는 방식, 선형 큐의 문제점을 보완
+//          단 가득찰 때와 비어있을 때 포인터가 같은 위치를 지정하기 때문에 이를 해결하기 위해 한 공간을 비워둔다.
+//        - 링크드 큐 : 연결 리스트로 구현, 큐의 길이를 쉽게 늘릴 수 있어 오버플로우가 발생하지 않음, 필요에 따라 환형으로 만들 수 도 있으며, 환형이 아니어도 삽입과 삭제가 제한되지 않아 편리
+//        - 큐가 꽉 차서 자료를 넣을 수 없는 경우 오버플로우(OverFlow), 비어있어 자료를 꺼낼 수 없는 경우 언더플로우(UnderFlow)
+
+
+//        큐 예시
+//        - 대기열 순서, 우선순위 작업 예약, 게임 '큐'잡기
+//        - BFS 구현시 처리해야 할 노드의 리스트를 저장하는 용도로 큐(Queue)사용
+//        - 캐시(Cache), 프로세스 관리, 운영체제의 테스크 스케줄링
+
+
+//        큐 기능
+//        - 인큐(Enqueue) : Back(tail, rear) 에 삽입연산, put, insert
+//        - 디큐(Dequeue) : Front(head) 에 삭제 연산, get, delete
+//        - peek
+//        - isEmpty
+//        - isFull
+
+        Queue<String> queue = null; // Queue 인터페이스를 (직간접적으로) 구현한 클래스가 많다
+//        queue = new PriorityQueue<>(); // 우선순위 큐 - 저장할 객체를 필수적으로 Comparable Interface 구현필요, Heap을 이용해 구현하는 것이 일반적, 요소에 null 허용X
+        queue = new LinkedList<>(); // 링크드 큐 - 끝에 요소 추가하기에 용이, List interface 구현, 요소에 null 허용
+//        queue = new Deque<String>() // 메서드를 구현해야 한다
+//        queue = new BlockingQueue<String>() // 얘도 구현필요,,,
+//        queue = new BlockingDeque<String>() // 구현필요,,
+//        queue = new TransferQueue<String>() // 구현,,으아아
+
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+        queue.add("d");
+        queue.add("e");
+
+        System.out.println(queue + " 왼쪽이 Front, 삭제나 반환 연산");
+
+        System.out.println("poll()연산 " + queue.poll() + queue); // empty 일때 null
+        System.out.println("remove()연산 " + queue.remove() + queue); // empty 일때 NoSuchElementException
+        System.out.println();
+
+        System.out.println("peek()연산 " + queue.peek() + queue);
+
+
+
+
+
+
+
+
+
+    }
 
 
 }
