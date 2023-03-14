4b660da01b58c0f10cb5ca7e56f8cc6ca0ed5b72
Hoony <xognsdl710@naver.com>
2023-03-15 01:41:21 +0900

    2023.03.15 코테연습 및 파일정리

---
+++ src/programmers/highScoreKit/StackQueue/StockPrice.java
@@ -0,0 +1,24 @@
+package programmers.highScoreKit.StackQueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class StockPrice { // 주식가격
+    // 초 단위로 기록된 주식가격
+    // 각 초의 입장에서 가격이 떨어지지 않은(자신보다 낮은 가격이 되는) 기간(사이 간격)은 몇 초인지 return
+    // 앞으로 연산(오른쪽 진행) - 재귀함수 이용해 보자!, 부분 정복 ver1
+    // 뒤로 연산(왼쪽 진행) -
+
+
+
+    public static void main(String[] args) {
+        int[] prices = {1,2,3,2,3}; // 길이 2~100000 (꽤 길다), 값 1~10000
+
+
+
+
+
+    }
+
+}
