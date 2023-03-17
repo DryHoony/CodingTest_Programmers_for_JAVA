0c528dc3fc5f6735371ee591139f8330a13955b4
Hoony <xognsdl710@naver.com>
2023-03-17 19:23:11 +0900

    2023.03.17 코테연습 및 파일정리

--- src/programmers/highScoreKit/Heap/DoublePriorityQueue.java
+++ src/programmers/highScoreKit/Heap/DoublePriorityQueue.java
@@ -2,15 +2,13 @@ package programmers.highScoreKit.Heap;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class DoublePriorityQueue { // 이중우선순위큐
 
     // 최소힙
     public static List<Integer> minHeap = new ArrayList<>();
 
-    public void minInput(int n){
+    public static void minInput(int n){
         minHeap.add(n);
         int c = minHeap.size()-1; // 자식노드 index
         int p = (c-1)/2; // 부모노드 index
@@ -26,7 +24,7 @@ public class DoublePriorityQueue { // 이중우선순위큐
         }
     }
 
-    public int minOutput(){
+    public static int minOutput(){
         int min = minHeap.get(0);
         int l = minHeap.size();
 
@@ -42,11 +40,11 @@ public class DoublePriorityQueue { // 이중우선순위큐
             lc = 2*p+1;
             rc = 2*p+2;
 
-            if(minHeap.get(p) > minHeap.get(lc) && minHeap.get(rc) > minHeap.get(lc)){ // 왼쪽 자식
+            if(minHeap.get(p) > minHeap.get(lc) && minHeap.get(rc) >= minHeap.get(lc)){ // 왼쪽 자식
                 minSwap(p,lc);
                 p = lc;
             }
-            else if(minHeap.get(p) > minHeap.get(rc) && minHeap.get(lc) > minHeap.get(rc)){ // 오른쪽 자식
+            else if(minHeap.get(p) > minHeap.get(rc) && minHeap.get(lc) >= minHeap.get(rc)){ // 오른쪽 자식
                 minSwap(p,rc);
                 p = rc;
             }
@@ -62,32 +60,39 @@ public class DoublePriorityQueue { // 이중우선순위큐
 
         return min;
     }
-    public void minSwap(int i, int j){ // i,j 위치 교환
+    public static void minSwap(int i, int j){ // i,j 위치 교환
         int n = minHeap.get(i);
         minHeap.set(i, minHeap.get(j));
         minHeap.set(j, n);
     }
 
-//    public void minHeapArrangeAfterRemoveMax(int n){ // 최댓값 제거 후 재정렬(반 정렬)
-//        int index = minHeap.indexOf(n);
-//        minHeap.remove(index);
-//
-//        for(int i=index; i<minHeap.size(); i++){
-//            if(i%2==0){ // 부모노드가 바뀐경우 >> 왼쪽자식에서 '오른쪽 자식'인 경우 >> 짝수
-//                // heap 반정렬 연산 - input 과 동일
-//                int c = i; // 자식노드
-//                int p = (c-1)/2; //부모노드
-//
-//
-//
-//            }
-//        }
-//    }
+    public static void minHeapArrangeAfterRemoveMax(int n){ // 최댓값 제거 후 재정렬(반 정렬)
+        int index = minHeap.indexOf(n);
+        minHeap.remove(index);
+
+        for(int i=index; i<minHeap.size(); i++){
+            if(i%2==0){ // 부모노드가 바뀐경우 >> 왼쪽자식에서 '오른쪽 자식'인 경우 >> 짝수
+                // heap 반정렬 연산 - input 과 동일
+                int c = i; // 자식노드
+                int p = (c-1)/2; //부모노드
+
+                while(c!=0){
+                    if(minHeap.get(p) > minHeap.get(c)){ // 자식노드가 더 작으면
+                        minSwap(p,c);
+                        c = p;
+                        p = (c-1)/2;
+                    }
+                    else break; // 힙 완성
+                }
+
+            }
+        }
+    }
 
     // 최대힙
     public static List<Integer> maxHeap = new ArrayList<>();
 
-    public void maxInput(int n){
+    public static void maxInput(int n){
         maxHeap.add(n);
         int c = maxHeap.size()-1; // 자식노드 index
         int p = (c-1)/2; // 부모노드 index
@@ -103,7 +108,7 @@ public class DoublePriorityQueue { // 이중우선순위큐
 
         }
     }
-    public int maxOutput(){
+    public static int maxOutput(){
         int max = maxHeap.get(0);
         int l = maxHeap.size();
 
@@ -119,11 +124,11 @@ public class DoublePriorityQueue { // 이중우선순위큐
             lc = 2*p+1;
             rc = 2*p+2;
 
-            if(maxHeap.get(p) < maxHeap.get(lc) && maxHeap.get(rc) < maxHeap.get(lc)){ // 왼쪽 자식
+            if(maxHeap.get(p) < maxHeap.get(lc) && maxHeap.get(rc) <= maxHeap.get(lc)){ // 왼쪽 자식
                 maxSwap(p,lc);
                 p = lc;
             }
-            else if(maxHeap.get(p) < maxHeap.get(rc) && maxHeap.get(lc) < maxHeap.get(rc)){ // 오른쪽 자식
+            else if(maxHeap.get(p) < maxHeap.get(rc) && maxHeap.get(lc) <= maxHeap.get(rc)){ // 오른쪽 자식
                 maxSwap(p,rc);
                 p = rc;
             }
@@ -139,22 +144,76 @@ public class DoublePriorityQueue { // 이중우선순위큐
 
         return max;
     }
-    public void maxSwap(int i, int j){ // i,j 위치 교환
+    public static void maxSwap(int i, int j){ // i,j 위치 교환
         int n = maxHeap.get(i);
         maxHeap.set(i, maxHeap.get(j));
         maxHeap.set(j, n);
     }
 
+    public static void maxHeapArrangeAfterRemoveMax(int n){ // 최솟값 제거 후 재정렬(반 정렬)
+        int index = maxHeap.indexOf(n);
+        maxHeap.remove(index);
+
+        for(int i=index; i<maxHeap.size(); i++){
+            if(i%2==0){ // 부모노드가 바뀐경우 >> 왼쪽자식에서 '오른쪽 자식'인 경우 >> 짝수
+                // heap 반정렬 연산 - input 과 동일
+                int c = i; // 자식노드
+                int p = (c-1)/2; //부모노드
+
+                while(c!=0){
+                    if(maxHeap.get(p) < maxHeap.get(c)){ // 자식노드가 더 크면
+                        maxSwap(p,c);
+                        c = p;
+                        p = (c-1)/2;
+                    }
+                    else break; // 힙 완성
+                }
+
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-        String[] operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        String[] operations = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
 
-//        Queue<Integer> queue1 = (Queue<Integer>) new DoublePriorityQueue(); // ClassCastException
+//        최대와 최소를 동시에 계산할 수 있는 '힙'이 필요 - 최대 힙, 최소 힙 두개로 표현하고 각각 힙에서 특정 값 제거 후 힙을 정렬하는 연산 구현
+//        minHeap, maxHeap 모두 사용
 
-        Queue<Integer> queue = new PriorityQueue<>(); // 최소 heap과 같다
-        // 최대와 최소를 동시에 계산할 수 있는 '힙'이 필요 - 최대 힙, 최소 힙 두개로 표현하고 각각 힙에서 특정 값 제거 후 힙을 정렬하는 연산 구현
+        for(String str:operations){
+            if(str.charAt(0) == 'I'){
+                System.out.println("I 연산");
 
-//        minHeap.remove(minHeap.indexOf(n));
+                minInput(Integer.parseInt(str.split(" ")[1]));
+                maxInput(Integer.parseInt(str.split(" ")[1]));
+
+                System.out.println("maxHeap = " + maxHeap +", minHeap = " + minHeap); // 확인출력
+
+            }
+
+            else if(maxHeap.size()!=0) { // 빈큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시
+                if (str.equals("D 1")) {
+                    System.out.println("최댓값 삭제 연산");
+                    minHeapArrangeAfterRemoveMax(maxOutput());
+                    System.out.println("maxHeap = " + maxHeap +", minHeap = " + minHeap); // 확인출력
+
+                }
+                else{
+                    System.out.println("최솟값 삭제 연산");
+                    maxHeapArrangeAfterRemoveMax(minOutput());
+                    System.out.println("maxHeap = " + maxHeap +", minHeap = " + minHeap); // 확인출력
+                }
+            }
+
+
+        }
+
+        // 모든 연산 처리후 [최댓값, 최솟값] return, 큐가 비면 [0,0] return
+        if(maxHeap.size()==0) System.out.println("return [0,0]");
+        else {
+            System.out.println(maxOutput() + " , " + minOutput());
+        }
+
 
 
 
