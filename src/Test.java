d45846b0e1104b0e873c5dd9aa32c8c5d9734a01
Hoony <xognsdl710@naver.com>
2023-03-20 22:13:54 +0900

    2023.03.20 코테연습 및 파일정리

--- src/Test.java
+++ src/Test.java
@@ -8,24 +8,19 @@ public class Test {
 
 
     public static void main(String[] args) {
-//        int[] test = {2,5,6,78,4,2,6,4,4,2,5,5,9,7,26,416,5,498,746,24,9,7,5,4,9,11,1,3,3,4,4,879,5};
-        int[] test = {9,8,7,6,10,70,5,4,3,2,1,0};
+        String x = "1234";
 
-//        MInHeapClass heap = new MInHeapClass();
-        MaxHeapClass heap = new MaxHeapClass();
+        x = x.substring(1);
+        System.out.println(x);
+        x = x.substring(1);
+        System.out.println(x);
+        x = x.substring(1);
+        System.out.println(x);
+        x = x.substring(1); // 마지막도 잘라지네!!
+        System.out.println(x);
 
 
-        for (int i:test){
-            heap.input(i);
-        }
 
-        Arrays.sort(test); // 오름차순
-        test = ReverseArray.reverse(test); // 내림차순
-
-        for(int i:test){
-            if(i != heap.peek()) System.out.print("다르다!! >> ");
-            System.out.println(i + " = " + heap.output());
-        }
 
     }
 
--- src/programmers/highScoreKit/Sort/MaximumNumber.java
+++ src/programmers/highScoreKit/Sort/MaximumNumber.java
@@ -8,35 +8,77 @@ public class MaximumNumber { // 가장 큰 수
     // 숫자 크기 비교연산시 (Comparator 따로 구현?) 최고자리 숫자 큰값, 같으면 다음 자릿수 큰 값
 
 
+    // 최고자릿수
+    public static int firstCipherOfNumber(Integer n){
+        while(n/10 != 0){ // 10으로 나눈 나머지가 0 일때까지 >> 한자리 수일때 까지
+            n /= 10; // 자릿수 땡김
+        }
+
+        return n;
+    }
+
     public static void main(String[] args) {
-        Integer[] numbers = {6,10,2}; // 원소는 0~1000, 길이는 1~100,000(많네)
-//        int[] numbers = {3,30,34,5,9};
+//        int[] numbers = {6,10,2}; // 원소는 0~1000, 길이는 1~100,000(많네)
+        int[] numbers = {3,30,34,5,9};
 
         // array 를 Integer[] 변경필요
+        Integer[] numberArray = new Integer[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numberArray[i] = numbers[i];
+        }
+
 
         /*
         * 정렬기준 정의
-        * 1. 최고자리 숫자 큰값
-        * 2. 다음 자릿수 큰 값 (다음 자릿수 가 존재하지 않으면 3.으로)
-        * 3. 자릿수 짧은 값
+        * 1. 최고자리 숫자 큰값 (0~9) 연산
+        * 2. 반복문 연산
+        * 2-1. 다음 자릿수로 넘어감(이하 자릿수 연산)
+        * 2-2. 자릿수가 큰 값 return, (만약 자릿수가 없다면 이전 자릿수로 연산) (34 > 3 > 30, '3'을 다음 자릿수 연산에서 33으로 취급)
+        * 2-3. 자릿수 연산에서 값이 같다면 while 문으로 2.1 부터 반복
+        * 2-4. 만약 둘중 자릿수가 더 없다면 '마지막 비교'로 연산종료
         * */
         // array 를 전체 정렬 vs '힙'을 이용한 반 정렬 (어짜피 최대값 순으로 뽑아낼테니)
-
-        Arrays.sort(numbers, new Comparator<Integer>() {
+        Arrays.sort(numberArray, new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                return 0;
+//                if (firstCipherOfNumber(o1) != firstCipherOfNumber(o2))
+//                    return firstCipherOfNumber(o2) - firstCipherOfNumber(o1); // 양수면 o2 return, 즉 큰 값 return
+//                else if()
+
+                String x = String.valueOf(o1);
+                String y = String.valueOf(o2);
+
+                if(x.charAt(0) != y.charAt(0)) // 최고자리 숫자 비교
+                    return y.charAt(0) - x.charAt(0); // 양수면 y return, 즉 큰 값 return
+                else{
+
+                    while(true){
+                        x = x.substring(1);
+                        y = y.substring(1);
+
+                        if(x.equals("")) return -1;
+                        else if(y.equals("")) return 1;
+                        else if (x.charAt(0) != y.charAt(0))
+                            return y.charAt(0) - x.charAt(0); // 양수면 y return, 즉 큰 값 return
+                    }
+
+                }
+
+
             }
         });
 
 
 
-
-        String answer = ""; // 정답이 너무 클 수 있으니 문자열로 바꿔 return
+        StringBuilder answer = new StringBuilder(); // 정답이 너무 클 수 있으니 문자열로 바꿔 return
         // answer 에 추가 연산
+        for(int n:numberArray){
+            answer.append(n);
+        }
 
         // answer return 값 확인
         System.out.println(answer);
+
 
 
 
