e5dfe91e26b4ab5155d885bb174f323f4c1d5b4b
Hoony <xognsdl710@naver.com>
2023-03-21 22:09:14 +0900

    2023.03.21 코테연습 및 파일정리

--- src/Test.java
+++ src/Test.java
@@ -8,16 +8,7 @@ public class Test {
 
 
     public static void main(String[] args) {
-        String x = "1234";
 
-        x = x.substring(1);
-        System.out.println(x);
-        x = x.substring(1);
-        System.out.println(x);
-        x = x.substring(1);
-        System.out.println(x);
-        x = x.substring(1); // 마지막도 잘라지네!!
-        System.out.println(x);
 
 
 
