d7175f65bc16059a80dee5f459e53f0030200d3f
Hoony <xognsdl710@naver.com>
2023-04-10 01:55:23 +0900

    2023.04.10 - 취미 연산

---
+++ src/playing/MapleEnhance.java
@@ -0,0 +1,22 @@
+package playing;
+
+public class MapleEnhance {
+
+
+    public static void main(String[] args) {
+        int[] cost = {0,300,600,900,1000,1200,1300,1600,2000,3000,4000};
+        int[] probability = {0,90,70,50,30,20,10,10,10,10,10};
+        int costI;
+        int totalCost = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            costI = cost[i] * 100 / probability[i];
+            totalCost += costI;
+            System.out.println(i + "번째 강화");
+            System.out.println("예상 비용 = " + costI + " / 총 예상비용 = " + totalCost);
+            System.out.println();
+
+        }
+
+    }
+}
