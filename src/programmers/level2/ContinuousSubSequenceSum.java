e2a76dd94f4276c4b692caf17b1dad4ce7fe4336
Hoony <xognsdl710@naver.com>
2023-08-17 10:26:44 +0900

    2023.08.17 - 코테 연습

---
+++ src/programmers/level2/ContinuousSubSequenceSum.java
@@ -0,0 +1,37 @@
+package programmers.level2;
+
+public class ContinuousSubSequenceSum { // 연속된 부분수열의 합
+    // https://school.programmers.co.kr/learn/courses/30/lessons/178870
+
+
+    public static void main(String[] args) {
+        // input1
+        int[] sequence={1,2,3,4,5};
+        int k = 7;
+
+        // input2
+//        int[] sequence={1,1,1,2,3,4,5};
+//        int k = 5;
+
+        // input3
+//        int[] sequence={2,2,2,2,2};
+//        int k = 6;
+
+        int[] answer = {0,0};
+        int sum=0;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
