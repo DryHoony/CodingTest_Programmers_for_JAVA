40e1cc390ae00c1bd03bf14c1e60406ba0c11faf
Hoony <xognsdl710@naver.com>
2023-08-20 16:21:56 +0900

    2023.08.20 - 코테 연습

--- src/Test.java
+++ src/Test.java
@@ -2,19 +2,6 @@ import java.util.*;
 
 public class Test {
 
-    public static void main(String[] args) {
-
-        // 알파벳 바꾸기
-
-        String name = "AABABAAA";
-
-        String[] arr = name.split("");
-
-        for(String a:arr){
-            System.out.println(a);
-        }
-
-    }
 
 
 
