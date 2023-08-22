a7877f1a9290d13120c106b1484cf5b51dde8930
Hoony <xognsdl710@naver.com>
2023-08-22 19:50:32 +0900

    2023.08.22 - 코테 연습

--- src/programmers/highScoreKit/dynamicProgramming/N_Expression.java
+++ src/programmers/highScoreKit/dynamicProgramming/N_Expression.java
@@ -11,6 +11,11 @@ public class N_Expression { // N으로 표현
         int count=0;
 
 
+        // 메모이제이션 f(n)
+        // 각 숫자에 대한 count
+        // f(n+1) = f(n-1) <= f(n) + 2 // N/N 으로 1 차이 만들 수 있다. 물론 더 간단한 방법으로 만들 수 도 있으므로 <=
+
+
         System.out.println("답은 = " + count);
     }
 
