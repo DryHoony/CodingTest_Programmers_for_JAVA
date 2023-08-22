cff81c9107e2415aae11034cfa3e677db503334b
Hoony <xognsdl710@naver.com>
2023-08-22 19:48:11 +0900

    2023.08.22 - 코테 연습

--- src/programmers/highScoreKit/dynamicProgramming/N_Expression.java
+++ src/programmers/highScoreKit/dynamicProgramming/N_Expression.java
@@ -3,7 +3,15 @@ package programmers.highScoreKit.dynamicProgramming;
 public class N_Expression { // N으로 표현
 
     public static void main(String[] args) {
+        int N = 5;
+        int number = 12; // 4
+        // N과 사칙연산을 통해 number 만들기, 나누기 연산에서 나머지는 무시
 
+        // 연산용 변수
+        int count=0;
+
+
+        System.out.println("답은 = " + count);
     }
 
 }
