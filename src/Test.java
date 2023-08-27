61b64abfec8454ab99f6962084abafb9fb3d89a8
Hoony <xognsdl710@naver.com>
2023-08-27 23:27:35 +0900

    2023.08.27 - 코테 연습

--- src/Test.java
+++ src/Test.java
@@ -2,42 +2,43 @@ import java.util.*;
 
 public class Test {
 
-
-    public static void main(String[] args) {
-        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+    public static void main(String[] args) { // 대각선 ver - 하나 빼고 다 틀림,,,ㅜㅜ
+        int n = 50; //세로 1~100
+        int m = 80; //가로 1~100
+        int[][] puddles = {{2,2}}; // 물이 잠긴 지역 0~10
 
         // 연산용 변수
-        int l = triangle.length;
-        ArrayList<Integer> previousLevel = new ArrayList<>();
-        ArrayList<Integer> nextLevel = new ArrayList<>();
-        int left;
-        int right;
+        int[][] map = new int[n+1][m+1]; // 초기값 0, index 는 m n 그대로
+        map[1][1] = 1; // 집 출발
 
-        // 첫째줄 등록
-        for(int n:triangle[l-1]){
-            previousLevel.add(n);
+        // 웅덩이 할당 -1
+        for(int[] p:puddles){
+            map[p[0]][p[1]] = -1;
         }
 
-        // 본연산
-        for (int i = l-2; i >=0 ; i--) {
-
-            // nextLevel 좌우 연산 한번에 시도 - max값 할당
-            for (int j = 0; j < i+1; j++) {
-                left = previousLevel.get(j) + triangle[i][j]; // 왼쪽
-                right = previousLevel.get(j+1) + triangle[i][j]; // 오른쪽
-                nextLevel.add(Math.max(left,right));
+        int j;
+        // 본 연산
+        for (int k = 3; k <= m+n; k++) {
+            // i+j = k
+            for (int i = 1; i < k; i++) {
+                j = k-i;
+                if(i>n || j>m) continue; // map 범위내 연산
+                System.out.println("["+i+","+j+"] 연산");
+//                map[i][j] = checkPuddle(map[i-1][j]) + checkPuddle(map[i][j-1]);
+                if(map[i][j] != -1) map[i][j] = (checkPuddle(map[i-1][j]) + checkPuddle(map[i][j-1])) % 1000000007;
             }
-            System.out.println("중간점검 i = "+i + " >> nextLevel = " + nextLevel);
-
-
-            previousLevel.clear();
-            previousLevel = (ArrayList<Integer>) nextLevel.clone();
-
-            nextLevel.clear();
+            System.out.println("다음 라인 >>");
 
         }
 
-        System.out.println("previousLevel 에 마지막남은 하나가 답" + previousLevel.get(0));
+
+        System.out.println();
+        System.out.println("전체 map 출력 확인");
+        for (int[] arr:map){
+            for(int a:arr) System.out.print(a +"  ");
+            System.out.println();
+        }
+        System.out.println("답은 = "+ map[n][m]);
     }
 
     // '프로그래머스' 에서는 array가 []로 주어진다.
@@ -48,6 +49,11 @@ public class Test {
 
         System.out.println(input);
         return input;
+    }
+
+    public static int checkPuddle(int x){
+        if(x==-1) return 0;
+        return x;
     }
 
 }
--- src/programmers/highScoreKit/dynamicProgramming/WayToSchool.java
+++ src/programmers/highScoreKit/dynamicProgramming/WayToSchool.java
@@ -2,13 +2,52 @@ package programmers.highScoreKit.dynamicProgramming;
 
 public class WayToSchool { // 등굣길
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // 가로줄 ver
         int n = 3; //세로 1~100
         int m = 4; //가로 1~100
         int[][] puddles = {{2,2}}; // 물이 잠긴 지역 0~10
 
         // 연산용 변수
-//        int count=0; // 최단경로의 갯수 1,000,000,007 로 나눈 나머지 (mod 를 언제 취할까?)
+        int[][] map = new int[n+1][m+1]; // 초기값 0, index 는 m n 그대로
+        map[1][1] = 1; // 집 출발
+
+        // 웅덩이 할당 -1
+        for(int[] p:puddles){
+            map[p[0]][p[1]] = -1;
+        }
+
+        // 첫줄 연산
+        for (int j = 2; j <= m; j++) {
+            if(map[1][j] != -1){ // 웅덩이가 아니면
+                map[1][j] = checkPuddle(map[1][j-1]);
+            }
+        }
+
+        // 본 연산
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // map[i][j] 연산
+                if(map[i][j] != -1) map[i][j] = (checkPuddle(map[i-1][j]) + checkPuddle(map[i][j-1]))%1000000007;
+            }
+        }
+
+        System.out.println();
+        System.out.println("전체 map 출력 확인");
+        for (int[] arr:map){
+            for(int a:arr) System.out.print(a +"  ");
+            System.out.println();
+        }
+        System.out.println("답은 = "+ map[n][m]);
+
+
+    }
+
+    public static void main0(String[] args) { // 대각선 ver - 하나 빼고 다 틀림,,,ㅜㅜ
+        int n = 3; //세로 1~100
+        int m = 4; //가로 1~100
+        int[][] puddles = {{2,2}}; // 물이 잠긴 지역 0~10
+
+        // 연산용 변수
         int[][] map = new int[n+1][m+1]; // 초기값 0, index 는 m n 그대로
         map[1][1] = 1; // 집 출발
 
@@ -25,7 +64,8 @@ public class WayToSchool { // 등굣길
                 j = k-i;
                 if(i>n || j>m) continue; // map 범위내 연산
                 System.out.println("["+i+","+j+"] 연산");
-
+//                map[i][j] = checkPuddle(map[i-1][j]) + checkPuddle(map[i][j-1]);
+                if(map[i][j] != -1) map[i][j] = (checkPuddle(map[i-1][j]) + checkPuddle(map[i][j-1])) % 1000000007;
             }
             System.out.println("다음 라인 >>");
 
@@ -38,9 +78,11 @@ public class WayToSchool { // 등굣길
             for(int a:arr) System.out.print(a +"  ");
             System.out.println();
         }
-//        count = count%1000000007;
-//        System.out.println("답은 = "+ count);
+        System.out.println("답은 = "+ map[n][m]);
     }
 
-
+    public static int checkPuddle(int x){
+        if(x==-1) return 0;
+        return x;
+    }
 }
