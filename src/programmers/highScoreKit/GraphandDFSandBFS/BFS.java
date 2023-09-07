4ca12e376a7e72690e61f2ef34017be27cf34b6d
Hoony <xognsdl710@naver.com>
2023-09-07 23:20:58 +0900

    2023.09.07 - 코테 연습

--- src/programmers/highScoreKit/GraphandDFSandBFS/BFS.java
+++ src/programmers/highScoreKit/GraphandDFSandBFS/BFS.java
@@ -1,13 +1,63 @@
 package programmers.highScoreKit.GraphandDFSandBFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS { // BFS Example
-
-
-
-
+    // 큐 사용
+    // 큐에 들어갈 때 방문체크
 
     public static void main(String[] args) {
+        BFSVer1.main(args);
+
 
     }
+
+    class BFSVer1 { // https://scshim.tistory.com/241
+        public static boolean[] visited = {false, false, false ,false ,false ,false ,false ,false, false};
+        // 간선을 2차원 배열로 표현
+        public static int[][] graph = {{},
+                {2, 3, 8},
+                {1, 7},
+                {1, 4, 5},
+                {3, 5},
+                {3, 4},
+                {7},
+                {2, 6, 8},
+                {1, 7}};
+
+        public static void bfs(int start){
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(start);
+
+            // 현재 노드를 방문 처리
+            visited[start] = true;
+
+            // 큐가 빌때까지 반복
+            while(!queue.isEmpty()){
+                int v = queue.poll();
+                System.out.print(v + " ");
+
+                // 인접한 노드 중 아직 방문하지 않은 원소들을 큐에 삽입
+                for(int i:graph[v]){
+                    if(visited[i] == false){
+                        queue.add(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+
+        }
+
+        public static void main(String[] args) {
+            int start = 1; // 시작 노드
+
+            System.out.print("BFS 탐색 >> ");
+            bfs(start);
+            System.out.println();
+
+        }
+    }
+
 
 }
--- src/programmers/highScoreKit/GraphandDFSandBFS/DFS.java
+++ src/programmers/highScoreKit/GraphandDFSandBFS/DFS.java
@@ -1,10 +1,96 @@
 package programmers.highScoreKit.GraphandDFSandBFS;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class DFS { // DFS Example
+    // 스택을 사용, 재귀를 사용
 
     public static void main(String[] args) {
+        DFSByRecursionVer1.main(args);
+        DFSByStackVer1.main(args);
+
 
     }
 
+    public static class DFSByRecursionVer1 { // https://scshim.tistory.com/241
+        public static boolean[] visited = {false, false, false ,false ,false ,false ,false ,false, false};
+
+        // 간선을 2차원 배열로 표현
+        public static int[][] graph = {{},
+                {2, 3, 8},
+                {1, 7},
+                {1, 4, 5},
+                {3, 5},
+                {3, 4},
+                {7},
+                {2, 6, 8},
+                {1, 7}};
+
+        public static void dfs(int v){
+            visited[v] = true; // 현재 노드 방문 처리
+            System.out.print(v + " "); // 방문노드 출력
+
+            for(int i : graph[v]){ // 인접 노드 탐색
+                if (visited[i]==false) dfs(i);
+            }
+        }
+
+        public static void main(String[] args) {
+//            System.out.println("어떻게 하면 내부 클래스 안의 main 메소드가 실행될까?");
+            int start = 1; // 탐색 시작 노드
+            System.out.print("재귀를 이용한 탐색 ver1, 간선 2차원 배열 >> ");
+            dfs(start);
+            System.out.println();
+        }
+    }
+
+    public static class DFSByStackVer1{ // https://scshim.tistory.com/241
+        public static boolean[] visited = {false, false, false ,false ,false ,false ,false ,false, false};
+
+        // 간선을 2차원 배열로 표현
+        public static int[][] graph = {{},
+                {2, 3, 8},
+                {1, 7},
+                {1, 4, 5},
+                {3, 5},
+                {3, 4},
+                {7},
+                {2, 6, 8},
+                {1, 7}};
+
+        public static void dfs(int start){
+            visited[start] = true; // 시작 노드 방문 처리
+            System.out.print(start + " "); // 방문 노드 출력
+
+            Deque<Integer> stack = new LinkedList<>();
+            stack.push(start); // 시작 노드를 스택에 입력
+
+            while(!stack.isEmpty()){
+                int now = stack.peek();
+
+                boolean hasNearNode = false; // 방문하지 않은 인접 노드 확인
+                for (int i:graph[now]){
+                    if(!visited[i]){ // 방문하지 않았으면
+                        hasNearNode = true;
+                        stack.push(i);
+                        visited[i] = true;
+                        System.out.print(i + " "); // 방문 노드 출력
+                        break; // 자식 노드를 모두 탐색-방문 하지 않고 Death First 가 되도록 한다.
+                    }
+                }
+                // 방문하지 않은 인접 노드가 없는 경우 해당 노드 꺼내기
+                if(hasNearNode == false) stack.pop();
+
+            }
+
+        }
+
+        public static void main(String[] args) {
+            System.out.print("Stack을 이용한 탐색 Ver1 >> ");
+            dfs(1); // 1에서 탐색 시작
+            System.out.println();
+        }
+    }
 
 }
