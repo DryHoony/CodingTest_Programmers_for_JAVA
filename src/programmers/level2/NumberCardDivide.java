86339a4a560d4172e51483601029cab9d1333ea5
Hoony <xognsdl710@naver.com>
2023-09-23 21:36:41 +0900

    2023.09.23 - 코테 연습

--- src/programmers/level2/NumberCardDivide.java
+++ src/programmers/level2/NumberCardDivide.java
@@ -3,6 +3,14 @@ package programmers.level2;
 public class NumberCardDivide { // 숫자카드나누기
 
     public static void main(String[] args) {
+        int[] arrayA = {10,17};
+        int[] arrayB = {5,20};
+        // array 길이 1~500,000 , 값 1~100,000,000
+
+
+
+
+
 
 
 
