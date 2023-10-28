4fc91a31e42bc5bdfa59a0a1907db45a5c940f31
Hoony <xognsdl710@naver.com>
2023-10-28 16:17:39 +0900

    2023.10.28 - 코테 연습

--- src/programmers/highScoreKit/greedyAlgorithm/BigNumberMaking.java
+++ src/programmers/highScoreKit/greedyAlgorithm/BigNumberMaking.java
@@ -7,34 +7,12 @@ public class BigNumberMaking { // 큰 수 만들기
         int k = 4; // 1~number.length
         // number 에서 4개를 제거 >> 가장 큰 수 return
         // number 의 숫자 순서를 유지
-
+        // 그리디하게 k번 연산하기는 너무 무겁지 않나?,,
 
         // 연산용 변수
-        int[] digitArray = new int[10]; // digit[i]는 i 숫자의 갯수
-        for (String str:number.split("")){
-            digitArray[Integer.parseInt(str)]++;
-        }
-
-
-        // 연산1 - digitArray 에서 작은수부터 k갯수 만큼 제거
-        for (int i = 0; i < 10; i++) {
-            if(digitArray[i] < k){
-                k -= digitArray[i];
-                digitArray[i] = 0;
-            }
-            else { // digitArray[i] == k
-                digitArray[i] = 0;
-                k = 0;
-                break;
-            }
-        }
-
-
-        // 연산2 - digitArray 를 큰 수부터 String 형태로 변환
         String answer = "";
-        for (int i = 9; i >= 0; i--) {
-            System.out.println(digitArray[i]);
-        }
+
+
 
 
         System.out.println("답은 = " + answer);
