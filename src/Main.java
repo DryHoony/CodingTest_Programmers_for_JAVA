469fba1cd807142462a7a6e72c1ea9a17860309c
Hoony <xognsdl710@naver.com>
2023-12-08 17:34:23 +0900

    2023 - 12 - 08 - 노트북 백업 후 IntelliJ 재설치 - github 연동 - 최초 확인용 커밋 - test git

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 //    위 블로그 글을 바탕으로 github에 연동
 //    커밋 : 파일을 추가하거나 변경 내용을 저장소에 저장하는 작업
 //    푸시 : 파일을 추가하거나 변경 내용을 원격 저장소에 업로드하는 작업
-
+// 12.08 test git
 
     public static void main(String[] args) {
         int n = 10;
