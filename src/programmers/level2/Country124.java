f7bc4da324151f39fd8f8a43852287a6d5a9a30e
Hoony <xognsdl710@naver.com>
2024-02-02 15:39:01 +0900

    2024 - 02 - 02 < 코테연습 > - 124 나라의 숫자 Lev2 - ver2 실패

--- src/programmers/level2/Country124.java
+++ src/programmers/level2/Country124.java
@@ -1,12 +1,14 @@
 package programmers.level2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Country124 { // 124 나라의 숫자
     // 숫자 124를 이용한 3진법 처럼 표현
 
-    public static void main(String[] args) {
-        int n = 4;
+    // ver1 - 3진법 변환 ver >> 자릿수가 달라지는 구간이 있다!! 실패!
+    public static void main1(String[] args) {
+        int n = 13;
 
         System.out.println("n = " + n);
 
@@ -45,4 +47,50 @@ public class Country124 { // 124 나라의 숫자
 
         System.out.println("답은 = " + answer);
     }
+
+    // ver2
+    public static void main(String[] args) {
+        int n = 2;
+//        System.out.println("n = " + n);
+//        System.out.println(change(n));
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print("i = " + i + " >> ");
+            System.out.println(change(i));
+        }
+
+    }
+
+    public static String change(int n){
+        String answerReverse = "";
+        int k;
+        n--;
+//        System.out.println("출력 test");
+
+        while(n>0){
+            k = n%3;
+            if(k==1){
+                answerReverse += "2";
+//                System.out.println("1 할당");
+            }else if (k==2){
+                answerReverse += "4";
+//                System.out.println("2 할당");
+            }else if (k==0){
+                answerReverse += "1";
+//                System.out.println("4 할당");
+            }
+
+            n /= 3;
+            n--;
+//            System.out.println("3나누고 남은 n = " + n);
+        }
+
+        String answer = "";
+        for (int i = answerReverse.length()-1; i >= 0; i--) {
+            answer += answerReverse.charAt(i);
+        }
+
+        return answer;
+    }
+
 }
