35368175d24a701b487362958a363380d09fe850
Hoony <xognsdl710@naver.com>
2024-02-05 14:58:13 +0900

    2024 - 02 - 05 < 코테연습 > - 땅따먹기 lev2 - 풀이중

---
+++ src/programmers/level2/LandEating.java
@@ -0,0 +1,22 @@
+package programmers.level2;
+
+public class LandEating { // 땅따먹기
+
+    public static void main(String[] args) {
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}}; // n행 4열
+        // n:1~100,000, 점수(값):1~100
+
+        // 연산용 변수
+        int answer = 0;
+        n = land.length;
+
+
+
+        System.out.println("답은 = " + answer);
+    }
+
+    static int n;
+    int[] bestIndex = new int[n];
+
+
+}
