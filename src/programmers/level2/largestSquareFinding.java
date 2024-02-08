0d43650147f8275b3799c1f2439de0076156e3d1
Hoony <xognsdl710@naver.com>
2024-02-08 10:38:41 +0900

    2024 - 02 - 08 < 코테연습 > - lev2 가장 큰 정사각형 찾기

---
+++ src/programmers/level2/largestSquareFinding.java
@@ -0,0 +1,16 @@
+package programmers.level2;
+
+public class largestSquareFinding { // 가장 큰 정사각형 찾기
+
+    public static void main(String[] args) {
+        int[][] board = new int[][]{{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};
+
+        // 연산용 변수
+        int answer = 0;
+
+
+
+
+        System.out.println("답은 = ");
+    }
+}
