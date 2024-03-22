cf757c22f628c0d26d8f55473f1a97dc80434232
Admin <Admin@DESKTOP-TK575EN>
2024-03-22 17:22:51 +0900

    2024/03/22
    - Eclipse : Commit, Push 테스트
    - 잔디 확인

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 public class Test2 {
 
     public static void main(String[] args) {
-    	String s = "5시 12분";
+    	String s = "5시 22분";
     	String s2 = "Commit & Push 확인";
 
     	
