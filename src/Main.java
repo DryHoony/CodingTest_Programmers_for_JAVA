59eb845ea8169ec3f5c50334f6477144fe0b9e9e
DryHoony <xognsdl710@naver.com>
2024-05-28 17:57:50 +0900

    2024-05-25

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
 //    위 블로그 글을 바탕으로 github에 연동
 //    커밋 : 파일을 추가하거나 변경 내용을 저장소에 저장하는 작업
 //    푸시 : 파일을 추가하거나 변경 내용을 원격 저장소에 업로드하는 작업
+	
+//	https://hyunipad.tistory.com/70
+//	이클립스 - 깃허브 연동
+//	https://dreamcoding.tistory.com/47
+//	이클립스에서 깃허브 로그인이 안될때 (토큰발행)
 
     public static void main(String[] args) {
 		
