6731ec3b89085cbc344cfa174fabbabeadbc9d87
Hoony <xognsdl710@naver.com>
2024-06-10 12:54:21 +0900

    2024-06-10 프로그래머스 Lv2 도넛과 막대 그래프 (2024 KAKAO Winter Internship) - solution2     solution1에서 수정중 - solution3     새로운 자료구조로 try

--- src/programmers/level2/DoughnutAndHistogram.java
+++ src/programmers/level2/DoughnutAndHistogram.java
@@ -1,6 +1,8 @@
 package programmers.level2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DoughnutAndHistogram { // 도넛과 막대 그래프
     // 크기 n에 대해
@@ -22,7 +24,25 @@ public class DoughnutAndHistogram { // 도넛과 막대 그래프
 //        edges = new int[][] {{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}};
 //        result = new int[] {4,0,1,2};
 
-        solution1(edges);
+        solution3(edges);
+    }
+
+    public static int[] solution3(int[][] edges){ // node 자료구조 ver
+        Map<Integer, Integer> nextNode = new HashMap<>();
+        Map<Integer, Integer> preNode = new HashMap<>();
+        ArrayList<int[]> duplicatedEdges = new ArrayList<>();
+
+        int[] answer = new int[4];
+
+        for (int[] edge:edges){
+            // node(next, pre)에 할당, 중복시 duplicatedEdges 에 할당
+
+        }
+
+        // duplicatedEdges 내에서 '생성노드' 찾기 >> answer[0]
+
+
+        return null;
     }
 
     static int[] answer;
@@ -45,6 +65,12 @@ public class DoughnutAndHistogram { // 도넛과 막대 그래프
 //                answer[1]++;
                 // 출력
                 System.out.println("도넛 출력 >> " + circlePrint);
+                // nodesFinished 연산 추가
+                while(nodesNotFinished[nextNode]){
+                    nodesNotFinished[nextNode] = false;
+                    nextNode = nodes[nextNode];
+                }
+
                 return true;
                 // nodes 초기화 X, duplicatedNode 연산에서 나머지 처리
             }
@@ -169,4 +195,140 @@ public class DoughnutAndHistogram { // 도넛과 막대 그래프
         System.out.println("8자 갯수 = " + answer[3]);
         return null;
     }
+
+    static boolean[] nodesNotFinished;
+    public static int[] solution2(int[][] edges){ // int[1000000] 사이즈에 다음 노드 표현
+        answer = new int[4]; // // 생성정점, 도넛 그래프의 수, 막대 그래프 수, 8자 그래프 수
+        nodes = new int[1000001]; // nodes[i]=j
+        nodesNotFinished = new boolean[1000001]; // true 이면 연산 미완료 상태
+        ArrayList<int[]> duplicatedEdges = new ArrayList<int[]>(); // 중복된 노드
+        ArrayList<int[]> duplicatedEdgesRemain = new ArrayList<int[]>(); // 남은 중복된 노드
+
+
+        // 1. edge 를 nodes 에 할당, 고리연산-도넛 >> nodes, duplicatedEdges, answer[1]
+        for(int[] edge:edges){
+
+            if(nodes[edge[0]] != 0)
+                duplicatedEdges.add(edge);
+            else
+            {
+                nodes[edge[0]] = edge[1]; // 할당
+                nodesNotFinished[edge[0]] = true;
+                // 순환 고리 연산 >> answer[1]
+                if(circle(edge[0]))
+                    answer[1]++;
+
+
+            }
+        }
+
+        // 2. duplicatedEdges 를 다시 nodes 에 할당, 고리연산-8자 >> duplicatedEdgesRemain, answer[1], answer[3]
+        for(int[] edge:duplicatedEdges){
+            // 순환 고리 연산
+            if(circle(edge[1], edge[0])){ // 8자고리인지, 생성노드 인지 구분 필요
+                answer[1]--; // 도넛 고리 2개로
+                answer[3]++; // 8자 고리 만듬
+            }
+            else {
+                duplicatedEdgesRemain.add(edge);
+            }
+        }
+
+//        System.out.println("duplicatedEdgesRemain = " + duplicatedEdgesRemain);
+        // 3. 생성노드 찾기, duplicatedEdges 에서 이미 연결된 고리(nodes[edge[1]])로 이어지면 생성노드! > answer[0]
+//        int firstNode = -1;
+        for(int[] edge:duplicatedEdges){
+            if(nodes[edge[1]] != 0){
+//                firstNode = edge[0];
+                answer[0] = edge[0];
+                break;
+            }
+        }
+//        System.out.println("생성노드 = " + firstNode);
+//        System.out.println("duplicatedEdgesRemain = " + duplicatedEdgesRemain);
+        System.out.println(duplicatedEdgesRemain.get(0)[0] + ", " + duplicatedEdgesRemain.get(0)[1]);
+        // 예제 1번의 [4,3] 막대를 못잡는다 >> 1. 에서 할당한 nodes 값이 사용됬는지 확인여부 flag 필요!
+
+        ArrayList<ArrayList<Integer>> barGraphs = new ArrayList<>();
+        ArrayList<Integer> newBar;
+        boolean newFlag;
+        // 4. 남은 duplicatedEdgesRemain 에서 막대 찾기>> answer[2]
+        // nodesFinished 값이 false 인 nodes 값 연산 필요
+        // 수정 필요!! >> 막대가 따로따로 구성될 경우 여러개로 쪼개질 수 있다.
+        for(int[] edge:duplicatedEdgesRemain){
+
+            if(edge[0]==answer[0] && nodes[edge[1]]!=0) continue; // 생성노드 무시 (연산에서 제끼기)
+            newFlag = true;
+
+            for(ArrayList<Integer> bar:barGraphs){
+                if(edge[1] == bar.get(0)) {
+                    bar.add(0,edge[0]); // bar 앞에 추가
+                    newFlag = false;
+                    break;
+                }
+                else if(bar.getLast() == edge[0]){
+                    bar.add(edge[1]); // bar 뒤에 추가
+                    newFlag = false;
+                    break;
+                }
+            }
+
+            if(newFlag){
+                newBar = new ArrayList<>();
+                newBar.add(edge[0]);
+                newBar.add(edge[1]);
+                barGraphs.add(newBar);
+            }
+
+        }
+
+        int[] edge;
+        // nodeFinished 값 false 인 nodes 추가
+        for (int i = 0; i < nodesNotFinished.length; i++) {
+            if(nodesNotFinished[i]){
+                System.out.println(i+"인덱스 미완료 상태 -> 추가 연산");
+                edge = new int[]{i, nodes[i]};
+
+                if(edge[0]==answer[0] && nodes[edge[1]]!=0) continue; // 생성노드 무시 (연산에서 제끼기)
+                newFlag = true;
+
+                for(ArrayList<Integer> bar:barGraphs){
+                    if(edge[1] == bar.get(0)) {
+                        bar.add(0,edge[0]); // bar 앞에 추가
+                        newFlag = false;
+                        break;
+                    }
+                    else if(bar.getLast() == edge[0]){
+                        bar.add(edge[1]); // bar 뒤에 추가
+                        newFlag = false;
+                        break;
+                    }
+                }
+
+                if(newFlag){
+                    newBar = new ArrayList<>();
+                    newBar.add(edge[0]);
+                    newBar.add(edge[1]);
+                    barGraphs.add(newBar);
+                }
+            }
+        }
+
+        // 막대기 이어지는 것 겹치기 or 생성노드 연결은 제외
+
+
+        answer[2] = barGraphs.size();
+        System.out.println("barGraphs = " + barGraphs);
+
+
+        System.out.println();
+        System.out.println("답 출력!");
+        System.out.println("생성 노드 = " + answer[0]);
+        System.out.println("도넛 갯수 = " + answer[1]);
+        System.out.println("막대 갯수 = " + answer[2]);
+        System.out.println("8자 갯수 = " + answer[3]);
+        return null;
+    } // 미완성 >> 시간초과
+
+
 }
