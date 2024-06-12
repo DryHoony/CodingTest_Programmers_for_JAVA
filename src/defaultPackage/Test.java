6cae9b23b7b18a628ba91bcd48931c522be948f6
Hoony <xognsdl710@naver.com>
2024-06-12 16:47:40 +0900

    2024-06-12 프로그래머스 Lv2 석유 시추   - Solution1 작성중 Test   - 테스트 연산 작성

--- src/defaultPackage/Test.java
+++ src/defaultPackage/Test.java
@@ -8,13 +8,38 @@ public class Test {
 
 	
 	public static void main(String[] args) {
-		Map<Integer,Integer> testMap = new HashMap<>();
+		ArrayList<int[]> testList = new ArrayList<>();
+		int[] changeArr = new int[5];
+		for (int i = 0; i < changeArr.length; i++) {
+			changeArr[i]=i;
+		}
+
+		testList.add(changeArr);
+		// 할당이후 changeArr 수정
+
+		System.out.println("덧셈연산");
+		for (int i = 0; i < changeArr.length; i++) {
+			changeArr[i]++;
+			changeArr[i]++;
+		}
+
+		for (int i:testList.get(0)){
+			System.out.print(i + "  ");
+		}
+		System.out.println();
+
+		System.out.println("두배연산");
+		for (int i = 0; i < changeArr.length; i++) {
+			changeArr[i]*=2;
+		}
+
+		for (int i:testList.get(0)){
+			System.out.print(i + "  ");
+		}
+		System.out.println();
 
 
-		testMap.put(1,1);
-		System.out.println(testMap.get(1));
-		System.out.println(testMap.get(2));
-//		int tryNull = testMap.get(2);
+
 	}
 	
 
--- src/programmers/level2/OilDrilling.java
+++ src/programmers/level2/OilDrilling.java
@@ -22,17 +22,46 @@ public class OilDrilling { // 석유 시추
         // case2
 //        land = new int[][] {{1, 0, 1, 0, 1, 1}, {1, 0, 1, 0, 0, 0}, {1, 0, 1, 0, 0, 1}, {1, 0, 0, 1, 0, 0}, {1, 0, 0, 1, 0, 1}, {1, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1}};
 //        result = 16;
+
+        solution(land);
     }
 
     public static int solution(int[][] land){
         int n = land.length; // 세로길이
         int m = land[0].length; // 가로길이
-        ArrayList<int[]>[] widthMassArray = new ArrayList[n]; // [i] 칸에 해당되는 withMass 는 [s,e]
-
 
         // step1 각 덩어리 구분 -> 각 덩어리 사이즈, 및 해당 칼럼
+        ArrayList<int[]>[] widthOilMassArray = new ArrayList[n]; // [i] 칸에 해당되는 withMass 는 [s,e]
+        int[] oil = new int[2];
+        boolean newFlag = true;
         // step1.1 land 가로 탐색 -> 가로 덩어리 표기
+        for (int i = 0; i < n; i++) { // 세로
+            System.out.println(i + "행 탐색");
+
+            for (int j = 0; j < m; j++) { // 가로 탐색
+
+                if(land[i][j] == 1){ // oil
+                    System.out.println("기름이다!");
+                    if(newFlag){ // oil 시작
+                        oil = new int[2]; // 여기서 미리 widthOilMassArray 에 할당?? >> 테스트
+                        oil[0] = j;
+                    }else{ // oil 계속
+                        oil[1] = j;
+                    }
+                }else { // oil X
+                    if(newFlag){
+                        continue;
+                    }else{ // oil 끝
+                        widthOilMassArray[i].add(oil);
+                        newFlag = true;
+                        System.out.println(i + "행의 oil = [" + oil[0] + "," + oil[1]+"]");
+                    }
+                }
+            }
+        }
+
         // step1.2 가로 덩어리의 세로 연결 -> 최종 덩어리 도출
+
 
 
 
